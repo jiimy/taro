206587728a2e147c6cdd89beb9a2fa3f2f769e6a
rudwnok <rudwnok123@naver.com>
2023-03-24 13:28:16 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { HashRouter, unstable_HistoryRouter as Router } from "react-router-dom";
-import store from "./redux/store";
-import history from "./hooks/useHistory";
 import { Provider } from "react-redux";
+import { unstable_HistoryRouter as Router } from "react-router-dom";
+import App from "./App";
+import history from "./hooks/useHistory";
+import "./index.css";
+import store from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
