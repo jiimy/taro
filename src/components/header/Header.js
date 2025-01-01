cedf10d61e1cfb0952c1f1357c4d78f9d8f04efe
rudwnok <rudwnok123@naver.com>
2025-01-01 18:38:28 +0900

    모바일 수정

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -41,6 +41,7 @@ const Header = () => {
         <div
           className={classNames("header-btn", {
             "is-show": mobileHeaderState,
+            "is-disabled": question?.question === ''
           })}
           onClick={() => setMobileHeaderState(!mobileHeaderState)}
         >
